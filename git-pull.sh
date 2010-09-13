516d15fbc58504a54319eed4cf8b9d67bbcb8432
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-09-13 19:02:23 +0000

    gettextize: git-pull "You asked to pull" message
    Gettextize the "You asked to pull" error message. This message
    interpolated $1 which won't work with eval_gettext for obvious
    reasons, so I needed to provide an alias for it.
    
    This also made the message easier to translate, and I added a
    TRANSLATORS comment to make it even easier.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- git-pull.sh
+++ git-pull.sh
@@ -156,9 +156,12 @@ Generally this means that you provided a wildcard refspec which had no
 matches on the remote end."; echo
 		fi
 	elif [ $# -gt 0 ] && [ "$1" != "$remote" ]; then
-		echo "You asked to pull from the remote '$1', but did not specify"
-		echo "a branch. Because this is not the default configured remote"
-		echo "for your current branch, you must specify a branch on the command line."
+		requested_remote=$1
+		# TRANSLATORS: $requested_remote will be a remote name, like
+		# "origin" or "avar"
+		eval_gettext "You asked to pull from the remote '\$requested_remote', but did not specify
+a branch. Because this is not the default configured remote
+for your current branch, you must specify a branch on the command line."; echo
 	elif [ -z "$curr_branch" ]; then
 		echo "You are not currently on a branch, so I cannot use any"
 		echo "'branch.<branchname>.merge' in your configuration file."
