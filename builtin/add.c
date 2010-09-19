6c95f19dbc7bed61940f197c4afc2213f0e77293
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-09-19 15:48:06 +0000

    gettextize: git-add "pathspec [...] did not match" message
    Gettextize the "pathspec '%s' did not match any files" message. A test
    in t3700-add.sh explicitly checked for this message. Change it to skip
    under GETTEXT_POISON=YesPlease.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/add.c
+++ builtin/add.c
@@ -449,7 +449,7 @@ int cmd_add(int argc, const char **argv, const char *prefix)
 					if (excluded(&dir, pathspec[i], DT_UNKNOWN))
 						dir_add_ignored(&dir, pathspec[i], strlen(pathspec[i]));
 				} else
-					die("pathspec '%s' did not match any files",
+					die(_("pathspec '%s' did not match any files"),
 					    pathspec[i]);
 			}
 		}
