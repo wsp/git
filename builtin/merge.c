5cd3549c58c1981fa87a04bd955b60bef7247b1e
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-09-19 15:20:50 +0000

    gettextize: git-merge "You have not concluded your merge" messages
    Gettextize the "You have not concluded your merge messages. A test in
    t3030-merge-recursive.sh explicitly checked for this message. Change
    it to skip the test under GETTEXT_POISON=YesPlease.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/merge.c
+++ builtin/merge.c
@@ -910,10 +910,10 @@ int cmd_merge(int argc, const char **argv, const char *prefix)
 		 * add/rm <file>', just 'git commit'.
 		 */
 		if (advice_resolve_conflict)
-			die("You have not concluded your merge (MERGE_HEAD exists).\n"
-			    "Please, commit your changes before you can merge.");
+			die(_("You have not concluded your merge (MERGE_HEAD exists).\n"
+			    "Please, commit your changes before you can merge."));
 		else
-			die("You have not concluded your merge (MERGE_HEAD exists).");
+			die(_("You have not concluded your merge (MERGE_HEAD exists)."));
 	}
 
 	resolve_undo_clear();
