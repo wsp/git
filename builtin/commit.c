6c3bfcff3f8e820957e90ff21f456b61164720cd
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-09-19 16:53:39 +0000

    gettextize: git-commit "enter the commit message" message
    Gettextize the "# Please enter the commit message for your changes."
    message. Several tests in t7500-commit.sh and t7502-commit.sh assume
    that this message starts with a newline. Change the tests to to skip
    under GETTEXT_POISON=YesPlease.
    
    These fail under GETTEXT_POISON=YesPlease because the poison editor
    message doesn't do the right thing with comments and line breaks, so
    these messages will be incorrectly broken up across lines.
    
    This test should not be skipped under a hypothetical future testing
    mode that tests Git under language locales, since those messages
    should all start with a newline like the original.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -656,19 +656,19 @@ static int prepare_to_commit(const char *index_file, const char *prefix,
 				git_path("MERGE_HEAD"));
 
 		fprintf(fp,
-			"\n"
-			"# Please enter the commit message for your changes.");
+			_("\n"
+			"# Please enter the commit message for your changes."));
 		if (cleanup_mode == CLEANUP_ALL)
 			fprintf(fp,
-				" Lines starting\n"
+				_(" Lines starting\n"
 				"# with '#' will be ignored, and an empty"
-				" message aborts the commit.\n");
+				" message aborts the commit.\n"));
 		else /* CLEANUP_SPACE, that is. */
 			fprintf(fp,
-				" Lines starting\n"
+				_(" Lines starting\n"
 				"# with '#' will be kept; you may remove them"
 				" yourself if you want to.\n"
-				"# An empty message aborts the commit.\n");
+				"# An empty message aborts the commit.\n"));
 		if (only_include_assumed)
 			fprintf(fp, "# %s\n", only_include_assumed);
 
