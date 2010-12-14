84c1a89588c598530c4b0d2297ddddea9ee68278
Thomas Rast <trast@student.ethz.ch>
2010-12-14 19:38:22 +0100

    branch: do not attempt to track HEAD implicitly
    With branch.autosetupmerge=always, 'HEAD' becomes a valid target for
    branch tracking.  This is a bit silly, and in fact the code already
    guards against it when attempting it explicitly:
    
      $ git checkout HEAD^0
      $ git branch -t foo
      fatal: Cannot setup tracking information; starting point is not a branch.
    
    Silently drop the HEAD candidate in the implicit (i.e. without -t
    flag) case, so that the branch starts out without an upstream.
    
    Signed-off-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- branch.c
+++ branch.c
@@ -176,8 +176,12 @@ void create_branch(const char *head,
 		break;
 	case 1:
 		/* Unique completion -- good, only if it is a real ref */
-		if (explicit_tracking && !strcmp(real_ref, "HEAD"))
-			die("Cannot setup tracking information; starting point is not a branch.");
+		if (!strcmp(real_ref, "HEAD")) {
+			if (explicit_tracking)
+				die("Cannot setup tracking information; starting point is not a branch.");
+			else
+				real_ref = NULL;
+		}
 		break;
 	default:
 		die("Ambiguous object name: '%s'.", start_name);
